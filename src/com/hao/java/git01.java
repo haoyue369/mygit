09dfcd79f4eccb1709b98a0a36a57fa955fc7257
haoyue369 <2786538785@qq.com>
2021-05-25 20:30:05 +0800

    第三次测试追加

--- src/com/hao/java/git01.java
+++ src/com/hao/java/git01.java
@@ -4,5 +4,6 @@ public class git01 {
     public static void main(String[] args) {
         System.out.println("这是测试");
         System.out.println("开始修改");
+        System.out.println("再次猜测是");
     }
 }
