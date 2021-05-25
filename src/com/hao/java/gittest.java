0e93f5201b475b55c93705b1a0b869ba1d4de9d7
haoyue369 <2786538785@qq.com>
2021-05-25 21:17:57 +0800

    不同的提交方式

--- src/com/hao/java/gittest.java
+++ src/com/hao/java/gittest.java
@@ -1,7 +1,7 @@
 package com.hao.java;
 
-public class Git {
+public class gittest {
     public static void main(String[] args) {
-        System.out.println("更新操作");
+        System.out.println("尝试解决分支");
     }
 }
